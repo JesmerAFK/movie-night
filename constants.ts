473d77c000cae42b13b8313ceba816b3b332e385
JesmerAFK <mayor8900@gmail.com>
2026-07-23 18:14:21 +0800

    Update BACKEND_URL to live Render backend https://movie-night-1-ruxf.onrender.com

--- constants.ts
+++ constants.ts
@@ -2,4 +2,4 @@ export const API_KEY = "95f2419536f533cdaa1dadf83c606027";
 export const BASE_URL = "https://api.themoviedb.org/3";
 export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/";
 export const POSTER_BASE_URL = "https://image.tmdb.org/t/p/w500";
-export const BACKEND_URL = "https://movie-night-backend-jxyc.onrender.com";
+export const BACKEND_URL = "https://movie-night-1-ruxf.onrender.com";
